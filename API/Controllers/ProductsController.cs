f77da2bf30fac41dee8ea85dc9c2d3b794e061bb
Alex <oleshko@tlu.ee>
2022-12-24 13:01:45 +0200

    Modify ProductsController to use Specifications for single/many product(s)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Core.Interfaces;
+using Core.Specifications;
 
 namespace API.Controllers
 {
@@ -28,7 +29,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _productsRepo.ListAllAsync();
+            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var products = await _productsRepo.ListAsync(spec);
 
             return Ok(products);
         }
@@ -36,7 +38,10 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _productsRepo.GetByIdAsync(id);
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            
+            return await _productsRepo.GetEntityWithSpec(spec);
+            // return await _productsRepo.GetByIdAsync(id);
         }
 
         [HttpGet("brands")]
