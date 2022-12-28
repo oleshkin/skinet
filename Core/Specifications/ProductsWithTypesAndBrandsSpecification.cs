9c36b869d14d40aed6e551d1dcab74c9bc4b18f6
Alex <oleshko@tlu.ee>
2022-12-28 21:51:52 +0200

    Bugfix: decimal sorting problem in sqlite workaround. Bugfix: priceDesc ordering was not ordering by Descending order

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -23,7 +23,7 @@ namespace Core.Specifications
                         AddOrderBy(p => p.Price);
                         break;
                     case "priceDesc":
-                        AddOrderBy(p => p.Price);
+                        AddOrderByDescending(p => p.Price);
                         break;
                     default:
                         AddOrderBy(n => n.Name);
